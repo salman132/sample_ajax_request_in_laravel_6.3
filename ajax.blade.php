2b9704362f5fa79da9d817a81b86718439a9d53f
Salman Rahman Auvi <36760223+salman132@users.noreply.github.com>
2020-01-14 10:28:44 +0600

    Update ajax.blade.php

--- ajax.blade.php
+++ ajax.blade.php
@@ -1,30 +1,41 @@
-    <script>
-        $(document).ready(function () {
+<?php
 
-            $.ajaxSetup({
+public function show(Request $request, $id){
+        $employees = Employee::where('department',$id)->get();
+        if($request->ajax()){
+            return view('admin.roaster.projects.employees',compact('employees'))->render();
+        }
 
-                headers: {
+    }
 
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
 
-                }
+?>
 
-            });
 
-            $("#city").change(function () {
-                var val = this.value;
+<script>
+
+     $('#dep_id').on('change',function () {
+                var dep_id = $(this).val()
 
                 $.ajax({
-                    type: 'GET',
-                    url: '/home',
-                    data:{
-                        val: val,
+                    type:'GET',
+                    url: '{{url("eroster/employee/department")}}/'+ dep_id,
+
+                    success:function (response) {
+                        console.log(response);
+                        $('#emp_id').html('');
+
+                        $('#emp_id').append(response);
+
                     },
-                    success:function(data){
-                        $(".remove").remove();
-                        $(".success").append('<div class="text-success remove">Your data is: '+ val + '</div>')
+                    error:function (xhr) {
+                        console.log(xhr)
                     }
+
                 })
+
             });
-        });
-    </script>
+
+
+
+</script>
