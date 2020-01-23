8163d5da441344b6d523e483b879e0c1ec401c05
Salman Rahman Auvi <36760223+salman132@users.noreply.github.com>
2020-01-23 11:33:28 +0600

    Update ajax.blade.php

--- ajax.blade.php
+++ ajax.blade.php
@@ -9,6 +9,26 @@ public function show(Request $request, $id){
     }
 
 
+    public function ajax(Request $request, $id){
+        if($request->ajax()){
+            $today = $id; //Getting Starting Date
+
+
+            $busy = array(); //Agents Who are really busy
+
+            $assigned_agent = AssignAgentToProject::where('service_start','<=',$today)->where('service_ends','>=',$today)->get();
+            foreach ($assigned_agent as $agent){
+                $busy[] = $agent->agent_id;
+            }
+
+
+            //Getting Free Agents
+            $agents = Employee::where('role_id',6)->where('status','active')->whereNotIN('id',$busy)->get();
+
+            return view('admin.roaster.assign.ajax.agents',compact('agents'))->render();
+        }
+    }
+
 ?>
 
 
@@ -35,6 +55,25 @@ public function show(Request $request, $id){
                 })
 
             });
+            
+            
+       $('#datepicker').focusout(function () {
+            var end_date = $(this).val();
+
+            $.ajax({
+                type: "GET",
+                url: "{{ url('eroster/agent-finder/') }}/" + end_date,
+
+                success:function (response) {
+                    $("#agentArea").html("");
+                    $("#agentArea").append(response);
+                    console.log(response)
+
+                },
+                error: function (xhr) {
+                    console.log(xhr)
+                }
+            })
 
 
 
